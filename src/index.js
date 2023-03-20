0104734551732f44feeaa2f58411a78f595b9d22
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-20 20:41:56 +0530

    creating server with express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-const http = require("http");
+const app = require("./server");
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.statusCode = 200;
-    res.end();
-  }
-});
-
-server.listen(3001, () => {
-  console.log(`server on http://localhost:3001`);
+app.listen(3001, () => {
+  console.log("server on http://localhost:3001");
 });
