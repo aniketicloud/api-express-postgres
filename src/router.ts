b003065cd5edf783968f3f6a86d23bc3400391f1
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-23 22:14:17 +0530

    defining routes

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.post("/product", () => {});
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.post("/update", () => {});
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update/:id", () => {});
+
+/**
+ * Update Point
+ */
+router.post("/updatepoint", () => {});
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => {});
