032a5f58b8ed4778edaf29c5ab7e9a080ed927be
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-24 17:35:16 +0530

    creating customer middleware and using middleware

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
+const customLogger = (message: string) => (req, res, next) => {
+  console.log(`Hello from ${message}`);
+  next();
+};
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(customLogger("Customer Support"));
 
 app.get("/", (req, res) => {
   console.log("hello from express");
@@ -15,4 +23,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", router);
+
 export default app;
