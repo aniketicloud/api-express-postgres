ac52f8f7567541ae40190ced038667b2c55975a5
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-24 21:48:59 +0530

    protecting routes '/api' is protected. '/' is not

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express();
 
@@ -15,13 +16,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(customLogger("Customer Support"));
-
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
   res.json({ message: "hello" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
 export default app;
