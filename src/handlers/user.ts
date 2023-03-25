56ee309aadfc512d6956b8de3948895a7f57df93
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-25 16:55:00 +0530

    Authenticating a User

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createUser = async (req, res) => {
   const user = await prisma.user.create({
@@ -11,4 +11,27 @@ export const createUser = async (req, res) => {
 
   const token = createJWT(user);
   res.json(token);
+};
+
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+  if (!user) {
+    res.status(401);
+    res.json({ message: "Username or Password is incorrect" });
+    return;
+  }
+
+  const isValid = await comparePasswords(req.body.password, user.password);
+  if (!isValid) {
+    res.status(401);
+    res.json({ message: "Username or Password is incorrect" });
+    return;
+  }
+
+  const token = createJWT(user);
+  res.json({ token });
 };
