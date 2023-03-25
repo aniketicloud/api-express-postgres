ddd610818083ba139a9869cabe6b1c61c9944f8e
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-25 14:55:14 +0530

    validating a Bearer token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -9,12 +9,30 @@ export const createJWT = (user) => {
   return token;
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
     res.status(401);
     res.json({ message: "not authorized" });
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not a valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error)
+    res.status(401);
+    res.json({ message: "not a valid user" });
     return;
   }
 };
