79b5ee3ad536bb92454d209b29d9c60be108a9c1
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-26 14:22:35 +0530

    Create & Update Product handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -17,11 +17,54 @@ export const getProducts = async (req, res) => {
 // get single product
 export const getOneProduct = async (req, res) => {
   // express.urlencoded is turning into an object
-  const id = req.param.id;
+  const id = req.params.id;
 
   const product = await prisma.product.findFirst({
     where: { id: id, belongsToId: req.user.id },
   });
 
   res.json({ data: product });
+};
+
+// create a new product
+export const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+// update a product
+export const updateProduct = async (req, res) => {
+  const updated = await prisma.product.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+
+  res.json({ data: updated });
+};
+
+// delete the product
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+  });
+
+  res.json({ data: deleted });
 };
