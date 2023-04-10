a140a53c454a7a53f01b784d55716ad82ead2e8d
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-04-10 13:48:54 +0530

    error handling for create user

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,24 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  console.log("hello from express");
-  res.status(200);
-  res.json({ message: "hello" });
+app.get("/", (req, res, next) => {
+  setTimeout(() => {
+    next(new Error("custom Async error thrown"));
+  }, 1);
 });
 
 app.use("/api", protect, router);
 app.post("/user", createUser);
 app.post("/signin", signin);
+
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "invalid input" });
+  } else {
+    res.status(500).json({ message: "oops, that's on us" });
+  }
+});
 
 export default app;
